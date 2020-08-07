112b3ae5344b7a6a28ed0942473484e5383d0072
Jonathan Heinrich <jonathan.heinrich@wheregroup.com>
2020-08-07 13:29:44 +0200

    dynamic legends add check url validity

--- src/Mapbender/CoreBundle/Resources/public/mapbender.element.legend.js
+++ src/Mapbender/CoreBundle/Resources/public/mapbender.element.legend.js
@@ -129,7 +129,11 @@
         getLegendUrl: function(layer) {
             if (layer.options.legend) {
                 var layerLegendUrl = layer.options.legend.url;
-                var legendUrls;
+                var legendUrls = [];
+
+                if(!this.validURL(layerLegendUrl)){
+                    layerLegendUrl = null;
+                }
 
                 if (layerLegendUrl && layerLegendUrl.includes("/mapproxy/")) {
                     legendUrls = this._rewriteHostUrl(layerLegendUrl);
@@ -211,6 +215,12 @@
                 'WIDTH': this.olMap.getSize()['w'],
                 'HEIGHT': this.olMap.getSize()['h']
             };
+        },
+
+        validURL: function(str) {
+            regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+
+            return regexp.test(str);
         },
 
         _appendDynamicLegendUrlParameter: function(legendUrl, params){
